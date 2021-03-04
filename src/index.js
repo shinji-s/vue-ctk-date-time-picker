c3799ab5e64652e520cb52e256191f6705a5d469
Shinji Suzuki <suzuki@shio-lab.com>
2021-03-04 13:15:54 +0900

    Update package references.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import VueCtkDateTimePicker from "./VueCtkDateTimePicker/index.vue";
+
+export default {
+ install(Vue, options) {
+  // Let's register our component globally
+  // https://vuejs.org/v2/guide/components-registration.html
+  Vue.component("VueCtkDateTimePicker", VueCtkDateTimePicker);
+ }
+};
+
